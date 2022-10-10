2f726e461eeac65b4b829403149b827f8ba5c98e
Kristina <40335@mail.ru>
2022-10-10 11:27:38 +0300

    add Task_2

--- Task_2.py
+++ Task_2.py
@@ -6,7 +6,7 @@ factors = []
 for i in range(2, int(number ** 0.5) + 2):
     while number % i == 0:
         factors.append(i)
-number /= i
+        number //= i
 if number != 1:
-    factors.append(int(number))
+    factors.append(number)
 print(f'Список простых множителей числа {N}: {factors}')
